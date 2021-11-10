480c04fafedf4eb657fa4efcd67384adff770db4
MohamedSolimann <mohamadico@gmail.com>
2021-11-10 16:47:00 +0200

    created order template

--- frontend/src/app/modules/cart/cart-page/cart-page.component.ts
+++ frontend/src/app/modules/cart/cart-page/cart-page.component.ts
@@ -8,7 +8,24 @@ import { DataTransferService } from 'src/app/data-transfer.service';
 })
 export class CartPageComponent implements OnInit {
   constructor(public dataTransfer: DataTransferService) {}
+
   public cartProducts: Array<any> = this.dataTransfer.cart;
+  public show: boolean = true;
 
   ngOnInit(): void {}
+
+  removeItemFromCart(productName: String) {
+    debugger;
+    for (let i = 0; i < this.cartProducts.length; i++) {
+      if (this.cartProducts[i].name === productName) {
+        this.cartProducts.splice(i, 1);
+        return;
+      }
+    }
+  }
+  checkCartIsEmpty() {
+    if (this.cartProducts.length !== 0) {
+      this.show = false;
+    }
+  }
 }
