3745d3b12b06b67c54ea0d35927712d1b0c7d63f
MohamedSolimann <mohamadico@gmail.com>
2021-11-15 15:39:54 +0200

    adjusted all products page routes in homepage/body

--- frontend/src/app/modules/core/header/header.component.ts
+++ frontend/src/app/modules/core/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Product } from "../../product/models/response";
-import { ProductBackendCallsService } from "../../product/services/product-backend-calls.service";
 import { NavigationService } from "../../../service/navigation.service";
 
 @Component({
--- frontend/src/app/modules/product/services/product-backend-calls.service.ts
+++ frontend/src/app/modules/product/services/product-backend-calls.service.ts
@@ -1,21 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ProductBackendCallsService {
   constructor(public myHttp: HttpClient) {}
   getAllProducts() {
-    return this.myHttp.get('http://localhost:8080/products', {
-      headers: { 'Content-Type': 'Application/Json' },
+    return this.myHttp.get("http://localhost:8080/products", {
+      headers: { "Content-Type": "Application/Json" },
     });
   }
   getProductByCategory(category: String) {
+    let page = 0;
+    let limit = 5;
     return this.myHttp.get(
-      `http://localhost:8080/products/category/${category}`,
+      `http://localhost:8080/products/category/${category}/${page}/${limit}`,
       {
-        headers: { 'Content-Type': 'Application/Json' },
+        headers: { "Content-Type": "Application/Json" },
       }
     );
   }
