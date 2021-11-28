80780f35d67bef5ad9678eda11ca9a840ca0163c
MohamedSolimann <mohamadico@gmail.com>
2021-11-28 19:43:06 +0200

    updated web server ip and rebuild the app

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  host: "18.157.177.129",
+  host: "18.194.243.229",
   AWS: {
     AccessKey: "AKIAV3KXYYWZSYLXWDNU",
     SecrectKey: "AGTm6hWih+pBZpnu8rDGmEFkMr56q2/LBWnJp64z",
