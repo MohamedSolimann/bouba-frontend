9ba89b3c4135537e6c4db2a412c9aba0bc6e9fb0
MohamedSolimann <masoliman28@gmail.com>
2021-12-28 11:56:22 +0200

    changed aws key for security and enviroments path in gitignore

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   host: "18.194.243.229",
   AWS: {
-    AccessKey: "AKIAV3KXYYWZUK2MPH6I",
-    SecrectKey: "4fjmET9IsnLzK+LAemlS1SU+CTGOOo/95K72ncyQ",
+    AccessKey: "AKIAV3KXYYWZ7SQEBKGX",
+    SecrectKey: "iDxwdnsxroMOcccUARSqKWFhzMsO2YvDkLKHqoNe",
   },
   port: "8080",
   protocol: "http",
