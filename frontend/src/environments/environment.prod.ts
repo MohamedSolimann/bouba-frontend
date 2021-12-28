99ed6b4e197e07f5e704fb463b24da1cff580bc8
MohamedSolimann <masoliman28@gmail.com>
2021-12-28 12:33:07 +0200

    test gitingore

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   host: "18.194.243.229",
-  AWS: {
-    AccessKey: "AKIAV3KXYYWZWJK6S3GB",
-    SecrectKey: "w59GA5XX9uyCSrcydyxGu9XZLTbyhRSlbyJ5zP+v",
-  },
+
   port: "8080",
   protocol: "http",
 
