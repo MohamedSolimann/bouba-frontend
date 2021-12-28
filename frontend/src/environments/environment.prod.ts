b52b487eed979150d4e7294b10d051074624df9e
MohamedSolimann <masoliman28@gmail.com>
2021-12-28 11:27:38 +0200

    changed aws key for security

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   host: "18.194.243.229",
   AWS: {
-    AccessKey: "AKIAV3KXYYWZSYLXWDNU",
-    SecrectKey: "AGTm6hWih+pBZpnu8rDGmEFkMr56q2/LBWnJp64z",
+    AccessKey: "AKIAV3KXYYWZUK2MPH6I",
+    SecrectKey: "4fjmET9IsnLzK+LAemlS1SU+CTGOOo/95K72ncyQ",
   },
-  port:"8080",
-  protocol : 'http',
+  port: "8080",
+  protocol: "http",
 
   production: true,
 };
